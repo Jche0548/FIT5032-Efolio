c5cc570c2b32121abed6753e0d91d39d88f95bf2
Jui-Che Chen <jche0548@student.monash.edu>
2025-08-16 23:27:42 +1000

    Week3 eFolio Activity

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,39 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import JSONLab from './components/JSONLab.vue' ;
-import TheWelcome from './components/TheWelcome.vue'
+import JSONLab from './components/JSONLab.vue'
 import MyName from './components/MyName.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
+  <!-- <JSONLab /> -->
+
+  <Form />
+
+  <!--
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <!-- <HelloWorld msg="You did it!" />
-       <MyName />  -->
-       <JSONLab />
+      <HelloWorld msg="You did it!" />
+      <MyName />
     </div>
   </header>
+  -->
 
-  <!-- <main>
+  <!--
+  <main>
     <TheWelcome />
-  </main> -->
+  </main>
+  -->
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
+header { line-height: 1.5; }
+.logo { display: block; margin: 0 auto 2rem; }
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  header { display: flex; place-items: center; padding-right: calc(var(--section-gap) / 2); }
+  .logo { margin: 0 2rem 0 0; }
+  header .wrapper { display: flex; place-items: flex-start; flex-wrap: wrap; }
 }
 </style>
